8ab4b068bc8a0fa8b8e92fc5fb9b9ec5e788a5c4
Maciej Nadzieja <maciej.nadzieja@gmail.com>
2013-09-03 23:46:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+bash
+====
+
+handy bash scripts
+
+csrf_login_check.sh
+---
+script that use curl to log in into webpage that use [csrf](http://en.wikipedia.org/wiki/Cross-site_request_forgery) token together with standard login and password.
+  
+usage: ./csrf_login_check.sh  
+output if logged in: OK  
+output if login failed: FAIL  
+
+
---
+++ csrf_login_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#script configuration
+host=http://127.0.0.1
+username=abc
+password=xyz
+afterLoginPage=$host/start_page
+grepString=logged
+cookiesFilename=cookies
+csrfVarName=login[_csrf_token]
+loginVarName=login[username]
+passwordVarName=login[password]
+
+#save cookies
+curl -s -c $cookiesFileName $host > /dev/null 2>&1
+#get csrf - maybe try something smarter than cut?
+csrf=$(curl -s -b $cookiesFilename $host | grep $csrfVarName'".*value=".*"' -o | cut -f3 -d"\"")
+#prepare POST data
+data=$(echo "$csrfVarName=$csrf&$loginVarName=$username&$passwordVarName=$password")
+#login
+curl -s -b $cookiesFilename -c $cookiesFilename -d $data -X POST $host > /dev/null 2>&1
+#get page after login and check for info about being logged in
+afterLogin=$(curl -s -b $cookiesFilename $afterLoginPage | grep $grepString)
+if [[ "$afterLogin" == *$username* ]] 
+then
+  echo 'OK'
+else
+  echo 'FAIL'
+fi
